32476ed7f5fe6c752bfb9622750599a1ba9cf76f
Adrien Aflalo <aflalo@google.com>
2022-11-19 21:09:42 +0100

    updated pipelines

--- app/index.js
+++ app/index.js
@@ -8,6 +8,5 @@ app.get('/', (req, res) => {
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
-  console.debug('test');
+  console.log(`hello world: listening on port ${port}`);
 });
