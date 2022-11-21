f15c2cb8679130d797ef287e1b21c0a21cf07621
Aflalo Argolis <admin@aflalo.altostrat.com>
2022-11-21 09:34:52 +0000

    edited text

--- app/index.js
+++ app/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'Adrien';
-  res.send(`Coucou ${name} !`);
+  const name = process.env.NAME || 'LVMH';
+  res.send(`Hello ${name} !`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
