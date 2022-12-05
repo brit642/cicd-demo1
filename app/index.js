b6dc7d1871a097c0a93ca856d41a35b047135cc0
Adrien Aflalo <aflalo@google.com>
2022-12-05 17:19:15 +0100

    updated string

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'LVMH';
+  const name = process.env.NAME || 'Adrien';
   res.send(`Hello ${name} !`);
 });
 
