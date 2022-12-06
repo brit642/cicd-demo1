6cb72a790a6fecc5bb725d20ad4db5d81d0e5f82
Aflalo Argolis <admin@aflalo.altostrat.com>
2022-12-06 10:47:42 +0000

    Updated name

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'Adrien';
+  const name = process.env.NAME || 'Infomil';
   res.send(`Hello ${name} !`);
 });
 
